ae7b4084081bf5f47cd6305a66b3e643727541f2
olaiam <s202283060@kfupm.edu.sa>
2026-04-08 11:03:37 +0300

    todo2

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -112,8 +112,25 @@ const app = express();
 app.listen(3000, () => {
   console.log("API running at http://localhost:3000");
 });
-// create express app instance to create web server
 
+// create express app instance to create web server
+app.get("/echo", (req, res) => {
+  const { name, age } = req.query;
+
+  if (!name || !age) {
+    return res.status(400).json({
+      ok: false,
+      error: "name & age required"
+    });
+  }
+
+  res.json({
+    ok: true,
+    name,
+    age,
+    msg: `Hello ${name}, you are ${age}`
+  });
+});
 
 
 // Query params: /echo?name=Ali&age=22
